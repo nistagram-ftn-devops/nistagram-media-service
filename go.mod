b11f36be8d23f806ef62f952ab1293e8a454d4e2
Boris Šuličenko <b.sulicenko@gmail.com>
2021-08-14 14:24:34 +0200

    Release v1.0.1
    * Merged PR 4: 66: Add image for post
    
    66: Add image for post
    
    User can upload image. This services handles images in a way
    that all of them are uploaded to cloudinary bucket.
    
    Related work items: #66
    
    * #27
    
    * #37
    
    * #39
    
    * #39
    
    * #39
    
    * #39
    
    * #41

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.16
 require (
 	github.com/cloudinary/cloudinary-go v1.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jmoiron/sqlx v1.3.3 // indirect
+	github.com/rs/cors v1.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -16,27 +16,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
+	"github.com/rs/cors"
 )
 
 type Media struct {
 	Id       string `db:"media_id"`
-	PostId   string `db:"post_id"`
 	ImageUrl string `db:"image_url"`
 }
 
 var dbClient *sqlx.DB
 
 func uploadImage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	postId := vars["id"]
-
-	log.Println("Started upload for post-id ", postId)
-
-	exists := checkIfMediaExists(postId)
-	if exists {
-		writeResponse(w, http.StatusBadRequest, "media-exists")
-		return
-	}
+	log.Println("Started upload")
 
 	file, handler, err := r.FormFile("file")
 	if err != nil {
@@ -55,17 +46,17 @@ func uploadImage(w http.ResponseWriter, r *http.Request) {
 	resp, err := cld.Upload.Upload(context.Background(), handler.Filename, uploader.UploadParams{})
 
 	if err != nil {
-		log.Println("Error while uploading image to cloudinary for post-id ", postId, err.Error())
+		log.Println("Error while uploading image to cloudinary", err.Error())
 		writeResponse(w, http.StatusInternalServerError, "image-cloudinary-upload-error")
 		return
 	}
 
 	imageUrl := resp.SecureURL
-	log.Println("Image successfully uploaded to cloudinary for post-id ", postId)
+	log.Println("Image successfully uploaded to cloudinary")
 	log.Println("Image URL is ", imageUrl)
 	os.Remove(handler.Filename)
 
-	media := saveMedia(Media{PostId: postId, ImageUrl: imageUrl})
+	media := saveMedia(Media{ImageUrl: imageUrl})
 
 	if media == nil {
 		writeResponse(w, http.StatusInternalServerError, "image-db-save-error")
@@ -74,31 +65,33 @@ func uploadImage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func checkIfMediaExists(postId string) bool {
-	query := "SELECT media_id, post_id, image_url FROM media WHERE post_id = ?"
+func getMediaByImageId(w http.ResponseWriter, r *http.Request) {
+	query := "SELECT media_id, image_url FROM media WHERE media_id = ?"
 	var media Media
+	vars := mux.Vars(r)
+	postId := vars["id"]
 	err := dbClient.Get(&media, query, postId)
 
-	if err == nil {
+	if err != nil {
 		log.Println("Media for post-id ", postId, " already exists.")
-		return true
+		writeResponse(w, http.StatusNotFound, "media-not-found")
+	} else {
+		writeResponse(w, http.StatusOK, media)
 	}
-
-	return false
 }
 
 func saveMedia(m Media) *Media {
-	query := "INSERT INTO media (post_id, image_url) VALUES (?, ?)"
-	result, err := dbClient.Exec(query, m.PostId, m.ImageUrl)
+	query := "INSERT INTO media (image_url) VALUES (?)"
+	result, err := dbClient.Exec(query, m.ImageUrl)
 
 	if err != nil {
-		log.Println("Error while inserting media into db for post-id ", m.PostId, err.Error())
+		log.Println("Error while inserting media into db", err.Error())
 		return nil
 	}
 	id, err := result.LastInsertId()
 
 	if err != nil {
-		log.Println("Error while getting last inserted media-id from db for post-id ", m.PostId, err.Error())
+		log.Println("Error while getting last inserted media-id from db", err.Error())
 		return nil
 	}
 
@@ -137,11 +130,27 @@ func main() {
 	dbClient = createDbConnection()
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/media/{id}", uploadImage).Methods(http.MethodPost)
+	router.HandleFunc("/api/media", uploadImage).Methods(http.MethodPost)
+	router.HandleFunc("/api/media/{id}", getMediaByImageId).Methods((http.MethodGet))
 	router.HandleFunc("/api/media", func(w http.ResponseWriter, r *http.Request) {
 		writeResponse(w, http.StatusOK, "Hello world")
 	}).Methods(http.MethodGet)
 
+	// headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	// originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	// methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 	log.Printf("Started nistagram-media-service on port %s", "8000")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", router))
+	// log.Fatal(http.ListenAndServe("0.0.0.0:8000", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+
+	handler := cors.Default().Handler(router)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"},
+		AllowCredentials: true,
+	})
+
+	// Insert the middleware
+	handler = c.Handler(handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", handler))
+
 }
